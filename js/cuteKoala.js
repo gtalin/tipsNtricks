ce90571b950cf451f4dde6673c0ba5e0e97b0238
gtalin <gtalin@users.noreply.github.com>
2017-01-16 17:53:58 +0000

    Cute Koala

--- js/cuteKoala.js
+++ js/cuteKoala.js
@@ -1,7 +1,11 @@
 //circle for face
 //circle for ears
-//circle for 
+//circle for eyes
 //lips
 //hair
 //blush
 //shadow ears
+//radial gradient for cheeks blush?
+//Lighter pink shade on nose
+//Made ear with ::after so that can add shadow
+//earlier it was made with border radius grey and background pink;
